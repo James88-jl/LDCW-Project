0f8c9da4abb7967380aa96a7044055d454664270
James88-jl <JAMES.ONG.YOU@student.mmu.edu.my>
2026-09-25 16:36:29 +0800

    Add initial Netflix Movie Finder program

--- project.cpp
+++ project.cpp
@@ -1,1 +1,32 @@
-hai
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int genre;
+
+	cout << "=== Simple Netflix Movie Finder ===\n";
+	cout << "Choose a genre:\n";
+	cout << "1. Action\n";
+	cout << "2. Comedy\n";
+	cout << "3. Science Fiction\n";
+	cout << "Enter your choice: ";
+	cin >> genre;
+
+	switch (genre) {
+		case 1:
+			cout << "Recommended movie: Extraction\n";
+			break;
+		case 2:
+			cout << "Recommended movie: The Intern\n";
+			break;
+		case 3:
+			cout << "Recommended movie: The Adam Project\n";
+			break;
+		default:
+			cout << "Invalid choice. Please choose 1, 2, or 3.\n";
+	}
+
+	return 0;
+}
